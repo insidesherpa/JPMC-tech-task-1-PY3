431fa82596d954758926566f8dec052e24d2b9a4
sebastian <srdesingweb@gmail.com>
2021-06-27 22:27:44 -0400

    updating code - Bonus Track

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(
+        getDataPoint(quote),
+        (
+          quote['stock'],
+          quote['top_bid']['price'],
+          quote['top_ask']['price'],
+          (quote['top_bid']['price'] + quote['top_ask']['price'])/2)
+        )
+        
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +25,33 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(
+        getDataPoint(quote),
+        (
+          quote['stock'],
+          quote['top_bid']['price'],
+          quote['top_ask']['price'],
+          (quote['top_bid']['price'] + quote['top_ask']['price'])/2)
+        )
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculatePriceBEqualToZero(self):
+    quotes = [
+      {'price_a': 54, 'price_b': 0},
+      {'price_a': 421.2, 'price_b': 0},
+    ]
+    """ ------------ Add the assertion below ------------ """
+    print("Ratio is:",
+    getRatio(1,0)
+    )
 
-
+    for quote in quotes:
+      self.assertEqual(
+        getRatio(quote['price_a'],quote['price_b']),
+        None
+      )
 
 if __name__ == '__main__':
     unittest.main()
