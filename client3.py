61bdd32ffd29a6d442d45813fdf5136a140038a3
Ishita Narsiker <ishitanarsiker@gmail.com>
2021-07-07 11:34:12 +0100

    Update client3.py

--- client3.py
+++ client3.py
@@ -30,30 +30,34 @@ QUERY = "http://localhost:8080/query?id={}"
 N = 500
 
 def getDataPoint(quote):
-	""" Produce all of the needed values to generate a datapoint """
-	""" ------------- Update this function ------------- """
+	""" Produces all of the needed values to generate a datapoint """
+
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = (bid_price + ask_price) / 2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
-	""" Get ratio of price_a and price_b """
-	""" ------------- Update this function ------------- """
-	""" Also create some unit tests for this function in client_test.py """
-	return 1
+  """ Gets ratio of price_a and price_b """
+  """ Also created some unit tests for this function in client_test.py """
+  if (price_b == 0):
+    # when price_b is 0 avoid throwing ZeroDivisionError
+    return
+  return price_a / price_b
 
 # Main
 if __name__ == "__main__":
-
-	# Query the price once every N seconds.
-	for _ in iter(range(N)):
-		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-		""" ----------- Update to get the ratio --------------- """
-		for quote in quotes:
-			stock, bid_price, ask_price, price = getDataPoint(quote)
-			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-		print ("Ratio %s" % getRatio(price, price))
+  
+  # Query the price once every N seconds.
+  for _ in iter(range(N)):
+    quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+    
+    """ ----------- Updated to get the ratio --------------- """
+    prices = {}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+      print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+      
+    print ("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
